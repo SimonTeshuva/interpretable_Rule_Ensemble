c608b61d212ca43a9967582b6d3c56a7359d8633
Mario Boley <mario.boley@gmail.com>
2019-12-16 14:20:56 +0200

    integration into Context class and switch to generator

--- orb/search.py
+++ orb/search.py
@@ -33,10 +33,41 @@ def merge_intersection(a, b):
     return res
 
 
+Node = namedtuple('Node', ['generator', 'closure', 'extension', 'gen_index', 'val', 'val_bound'])
+value = attrgetter("val")
+
+
 class Context:
     """
     Formal context, i.e., a binary relation between a set of objects and a set of attributes.
     """
+
+    @staticmethod
+    def from_tab(table):
+        """
+        Converts an input table where each row represents an object into
+        a formal context (which uses column-based representation).
+
+        Uses Boolean interpretation of table values to determine attribute
+        presence for an object.
+
+        For instance:
+
+        >>> table = [[0, 1, 0, 1],
+        ...          [1, 1, 1, 0],
+        ...          [1, 0, 1, 0],
+        ...          [0, 1, 0, 1]]
+        >>> ctx = Context.from_tab(table)
+        >>> list(ctx.extension([0, 2]))
+        [1, 2]
+
+        :param table:
+        :return:
+        """
+        m = len(table)
+        n = len(table[0])
+        extents = [SortedSet([i for i in range(m) if table[i][j]]) for j in range(n)]
+        return Context(list(range(n)), list(range(m)), extents)
 
     def __init__(self, attributes, objects, extents):
         self.attributes = attributes
@@ -53,40 +84,83 @@ class Context:
         if not intent:
             return range(len(self.objects))
 
-        # result = self.extents[intent[0]][:]
-        # for i in intent[1:]:
-        #     result = merge_intersection(result, self.extents[i])
-
         result = SortedSet.intersection(*map(lambda i: self.extents[i], intent))
 
         return result
 
+    def bfs(self, f, g):
+        """
+        A first example with trivial objective and bounding function is as follows. In this example
+        the optimal extension is the empty extension, which is generated via the
+        the lexicographically smallest and shortest generator [0, 3].
+        >>> table = [[0, 1, 0, 1],
+        ...          [1, 1, 1, 0],
+        ...          [1, 0, 1, 0],
+        ...          [0, 1, 0, 1]]
+        >>> ctx = Context.from_tab(table)
+        >>> search = ctx.bfs(lambda e: -len(e), lambda e: 1)
+        >>> max(search, key=value)
+        Node(generator=[0, 3], closure=[0, 2, 3], extension=SortedSet([]), gen_index=3, val=0, val_bound=1)
 
-def ctx_from_tab(table):
-    """
-    Converts an input table where each row represents an object into
-    a formal context (which uses column-based representation).
+        Let's use more realistic objective and bounding functions based on values associated with each
+        object (row in the table).
+        >>> values = [-1, 1, 1, -1]
+        >>> f = lambda e: sum((values[i] for i in e))/4
+        >>> g = lambda e: sum((values[i] for i in e if values[i]==1))/4
+        >>> search = ctx.bfs(f, g)
+        >>> for n in search:
+        ...     print(n)
+        Node(generator=[], closure=[], extension=range(0, 4), gen_index=-1, val=0.0, val_bound=inf)
+        Node(generator=[0], closure=[0, 2], extension=SortedSet([1, 2]), gen_index=0, val=0.5, val_bound=0.5)
 
-    Uses Boolean interpretation of table values to determine attribute
-    presence for an object.
+        :param f: objective function
+        :param g: bounding function satisfying that g(I) >= max {f(J): J >= I}
+        """
+        n = len(self.attributes)
+        boundary = deque()
+        full = self.extension([])
+        root = Node([], [], full, -1, f(full), inf)
+        opt = root
+        boundary.append(root)
 
-    For instance:
+        def refinement(node, i):
+            if i in node.closure:
+                return None
 
-    >>> table = [[0, 1, 0, 1],
-    ...          [1, 1, 1, 0],
-    ...          [1, 0, 1, 0],
-    ...          [0, 1, 0, 1]]
-    >>> ctx = ctx_from_tab(table)
-    >>> list(ctx.extension([0, 2]))
-    [1, 2]
+            generator = node.generator + [i]
+            extension = node.extension & self.extents[i]
 
-    :param table:
-    :return:
-    """
-    m = len(table)
-    n = len(table[0])
-    extents = [SortedSet([i for i in range(m) if table[i][j]]) for j in range(n)]
-    return Context(list(range(n)), list(range(m)), extents)
+            val = f(extension)
+            bound = g(extension)
+
+            if bound < opt.val:
+                return None
+
+            closure = []
+            for j in range(0, i):
+                if j in node.closure:
+                    closure.append(j)
+                    continue
+
+                if extension >= self.extents[j]:
+                    return None
+
+            closure.append(i)
+
+            for j in range(i + 1, n):
+                if extension >= self.extents[j]:
+                    closure.append(j)
+
+            return Node(generator, closure, extension, i, val, bound)
+
+        while boundary:
+            current = boundary.popleft()
+            yield current
+            for a in range(current.gen_index + 1, n):
+                child = refinement(current, a)
+                if child:
+                    opt = max(opt, child, key=value)
+                    boundary.append(child)
 
 
 def cov_squared_dev(labels):
@@ -173,78 +247,3 @@ def cov_mean_bound(labels, f):
 
     return bound
 
-
-Node = namedtuple('Node', ['generator', 'closure', 'extension', 'gen_index', 'val', 'val_bound'])
-
-
-def bfs(f, g, ctx):
-    """
-    A first example with trivial objective and bounding function is as follows. In this example
-    the optimal extension is the empty extension, which is generated via the
-    the lexicographically smallest and shortest generator [0, 3].
-    >>> table = [[0, 1, 0, 1],
-    ...          [1, 1, 1, 0],
-    ...          [1, 0, 1, 0],
-    ...          [0, 1, 0, 1]]
-    >>> ctx = ctx_from_tab(table)
-    >>> bfs(lambda e: -len(e), lambda e: 1, ctx)
-    Node(generator=[0, 3], closure=[0, 2, 3], extension=SortedSet([]), gen_index=3, val=0, val_bound=1)
-
-    >>> values = [-1, 1, 1, -1]
-    >>> f = lambda e: sum((values[i] for i in e))/4
-    >>> g = lambda e: sum((values[i] for i in e if values[i]==1))/4
-    >>> bfs(f, g, ctx)
-    Node(generator=[0], closure=[0, 2], extension=SortedSet([1, 2]), gen_index=0, val=0.5, val_bound=0.5)
-
-    :param f: objective function
-    :param g: bounding function satisfying that g(I) >= max {f(J): J >= I}
-    :param ctx: the context defining the concept lattice over which the search is performed
-    :return:
-    """
-    value = attrgetter("val")
-    n = len(ctx.attributes)
-    boundary = deque()
-    full = ctx.extension([])
-    root = Node([], [], full, -1, f(full), inf)
-    opt = root
-    boundary.append(root)
-
-    def refinement(node, i):
-        if i in node.closure:
-            return None
-
-        generator = node.generator + [i]
-        extension = node.extension & ctx.extension([i])
-
-        val = f(extension)
-        val_bound = g(extension)
-
-        if val_bound < opt.val:
-            return None
-
-        closure = []
-        for j in range(0, i):
-            if j in node.closure:
-                closure.append(j)
-                continue
-
-            if extension >= ctx.extension([j]):
-                return None
-
-        closure.append(i)
-
-        for j in range(i + 1, n):
-            if extension >= ctx.extension([j]):
-                closure.append(j)
-
-        return Node(generator, closure, extension, i, val, val_bound)
-
-    while boundary:
-        current = boundary.popleft()
-        for a in range(current.gen_index + 1, n):
-            child = refinement(current, a)
-            if child:
-                opt = max(opt, child, key=value)
-                boundary.append(child)
-
-    return opt
