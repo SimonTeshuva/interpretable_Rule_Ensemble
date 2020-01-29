e5799c85ea15fe6cb3eff54fdd57d2e0b5fcd518
SimonTeshuva <17869051+SimonTeshuva@users.noreply.github.com>
2020-01-29 23:27:17 +1100

    updates to experiments

--- orb/experiments.py
+++ orb/experiments.py
@@ -6,7 +6,7 @@ from orb.xgb_functions import generate_xgb_model
 from orb.xgb_functions import count_nodes
 
 
-def exp(fn, target=None, without=[], target_labels = [], path="", model_class="xgboost", clear_results=False): # add model type in results
+def exp(fn, target=None, without=[], target_labels = [], path="", model_class="xgboost", clear_results=False, k_vals = [3], d_vals = [3], ld_vals = [10]):
     """
     >>> fn = "halloween_candy_ranking"
     >>> target = "winpercent"
@@ -22,16 +22,16 @@ def exp(fn, target=None, without=[], target_labels = [], path="", model_class="x
     :return:
     """
     if clear_results:
-        res_df = pd.DataFrame(columns=['dataset', 'target', 'no_feat', 'no_rows', 'tr_RMSE', 'te_RMSE', 'model_complexity'])
+        res_df = pd.DataFrame(columns=['dataset', 'target', 'no_feat', 'no_rows', 'tr_RMSE', 'te_RMSE', 'model_class', 'model_complexity', 'k', 'd', 'l'])
     else:
         res_df = pd.read_pickle("./results.pkl")  # open old results
 
-    [X_train, Y_train, X_test, Y_test], target_name = prep_data(fn, target, without, target_labels)
+    [X_train, Y_train, X_test, Y_test], target_name = prep_data(fn, target, without)
     data = [X_train, Y_train, X_test, Y_test]
 
-    for k in [3,10]: #[3,10]
-        for d in [1,3,5]: #[1,3,5]
-            for ld in [1,10,100]: # [1,10,100]
+    for k in k_vals:
+        for d in d_vals:
+            for ld in ld_vals:
                 # alternative, don't do with if block, feed model class as function input rather than model class's name
                 if model_class == "xgboost":
                     model, train_RMSE, test_RMSE = generate_xgb_model(k, d, ld, data)
@@ -47,18 +47,19 @@ def exp(fn, target=None, without=[], target_labels = [], path="", model_class="x
 
                 exp_res = {"dataset": fn, "target": target_name, "no_feat": len(X_train.columns),
                            "no_rows": Y_train.shape[0] + Y_test.shape[0], "tr_RMSE": train_RMSE, "te_RMSE": test_RMSE,
-                           "model_complexity": model_complexity}
+                           'model_class': model_class, "model_complexity": model_complexity, 'k': k, 'd': d, 'l': ld}
                 res_df = res_df.append(exp_res, ignore_index=True)
 
-    res_df.to_pickle("./results.pkl") #save current results table
+    res_df.to_pickle("./results.pkl", protocol=4) #save current results table
 
-    testing = True
+    testing = False
     if testing == True:
         read_results()
 
 
 def read_results():
     res_df = pd.read_pickle("./results.pkl") #open old results
+    res_df.to_csv('results.csv')
     pd.set_option('display.max_columns', 10)
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         print(res_df)
@@ -75,7 +76,7 @@ def dataset_signature(dataset_name):
             "gdp_vs_satisfaction": ("Satisfaction", ["Country"], []),
             "halloween_candy_ranking": ("winpercent", ['competitorname'], ['very bad', 'bad', 'average', 'good','very good']),
             "metacritic_games": ("user_score", ["GameID",	"name",	"players",	"attribute",	"release_date",	"link"], []),
-            "random_regression": ("", [], []), # "" not appropriate as null input, potentiall swap to None
+            "random_regression": (None, [], []), # "" not appropriate as null input, potentiall swap to None
             "red_wine_quality": ("quality", [], []),
             "suicide_rates": ("suicides/100k pop", ["suicides_no","population","HDI for year"], []),
             "titanic": ("Survived", ['PassengerId', 'Name', 'Ticket', 'Cabin'], []),
@@ -88,15 +89,15 @@ def dataset_signature(dataset_name):
     dataset_info = data[dataset_name]
     target = dataset_info[0]
     without = dataset_info[1]
-    target_labels = dataset_info[2]
-    return target, without, target_labels
+    return target, without
 
 
-def exp_on_all_datasets(datasets):
+def exp_on_all_datasets(datasets, k_vals = [3], d_vals = [3], ld_vals = [10]):
     first = True
     for fn in datasets:
-        target, without, target_labels = dataset_signature(fn)
-        exp(fn, target, without, target_labels, path="", model_class="xgboost", clear_results=first)
+        print(fn)
+        target, without = dataset_signature(fn)
+        exp(fn, target, without, path="", model_class="xgboost", clear_results=first, k_vals=k_vals, d_vals=d_vals, ld_vals=ld_vals)
         first = False
 
 
@@ -108,12 +109,17 @@ if __name__ == "__main__":
                  "who_life_expectancy", "world_happiness_indicator"]
 
     small_datasets = ["advertising", "halloween_candy_ranking", "random_regression", "red_wine_quality",
-                "titanic", "us_minimum_wage_by_state", "used_cars", "wages_demographics"] # small
+                      "titanic", "used_cars", "wages_demographics"] # small
+
+    # "us_minimum_wage_by_state" causing problems. suspect its the columns with ... and (b) entries in them
 
     # "gdp_vs_satisfaction": raise XGBoostError(py_str(_LIB.XGBGetLastError()))
     # xgboost.core.XGBoostError: [16:34:12] D:\Build\xgboost\xgboost-0.90.git\src\metric\rank_metric.cc:200: Check failed: !auc_error: AUC: the dataset only contains pos or neg samples
 
     debugging_datasets = ["advertising", "titanic"]
-    exp_on_all_datasets(debugging_datasets)
-    #read_results()
-
+    k_vals = [1, 3, 10]
+    d_vals = [1, 3, 5]
+    ld_vals = [1, 10, 100]
+#    exp_on_all_datasets(small_datasets)
+    exp_on_all_datasets(small_datasets, k_vals, d_vals, ld_vals)
+    read_results()
